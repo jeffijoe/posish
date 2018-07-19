8348da8e676aaeb10f5f145099b9206bd6fba3b6
Jeff Hansen <jeffijoe@hotmail.com>
2018-07-19 09:56:47 +0200

    Use production Inferno build

--- config-overrides.js
+++ config-overrides.js
@@ -5,12 +5,18 @@ module.exports = function(config, env) {
 
   // Use Inferno instead of React.
   config.resolve = Object.assign({}, config.resolve, {
-    alias: Object.assign({}, config.resolve.alias, {
-      inferno: __dirname + "/node_modules/inferno/dist/index.dev.esm.js",
-      react: "inferno-compat",
-      "react-dom": "inferno-compat",
-      "mobx-react": "inferno-mobx"
-    }),
+    alias: Object.assign(
+      {},
+      config.resolve.alias,
+      env !== "production" && {
+        inferno: __dirname + "/node_modules/inferno/dist/index.dev.esm.js"
+      },
+      {
+        react: "inferno-compat",
+        "react-dom": "inferno-compat",
+        "mobx-react": "inferno-mobx"
+      }
+    ),
     modules: [...config.resolve.modules, "src"]
   });
   return config;
